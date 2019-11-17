1895ff6f1ee14bc888d576e471a8bb2c6ccc8b07
mateus009 <37670583+mateus009@users.noreply.github.com>
2019-11-16 22:09:37 -0300

    teste3

--- StreetSecurityNow/app/src/main/java/com/example/streetsecuritynow/data/LoginDataSource.kt
+++ StreetSecurityNow/app/src/main/java/com/example/streetsecuritynow/data/LoginDataSource.kt
@@ -1,17 +1,27 @@
 package com.example.streetsecuritynow.data
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.example.streetsecuritynow.data.model.LoggedInUser
+import okhttp3.*
 import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
 class LoginDataSource {
 
+    private val client = OkHttpClient()
     fun login(username: String, password: String): Result<LoggedInUser> {
         try {
             // TODO: handle loggedInUser authentication
-            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
+            fetchJson();
+            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "teste")
             return Result.Success(fakeUser)
         } catch (e: Throwable) {
             return Result.Error(IOException("Error logging in", e))
@@ -20,6 +30,27 @@ class LoginDataSource {
 
     fun logout() {
         // TODO: revoke authentication
+    }
+    fun fetchJson(){
+
+        val url = "https://localhost:44335/api/Default"
+
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("${e?.message}")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response?.body?.string()
+                println(body)
+
+            }
+        })
+
     }
 }
 
